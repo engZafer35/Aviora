b92d483031e7d2da27b0c4929cc4e747adc91e64
engZafer35 <eng.zafer.s@gmail.com>
2026-03-17 09:53:05 +0300

    [DEV]-moved some fs files under midd_fs

--- Middleware/CycloneTcp/common/fs_port.h
+++ Middleware/CycloneTcp/common/fs_port.h
@@ -119,12 +119,6 @@ typedef struct
 //Keil RL-FlashFS port?
 #elif defined(USE_RL_FS)
    #include "fs_port_rl_fs.h"
-//littleFS port?
-#elif defined(USE_LITTLEFS)
-   #include "fs_port_littlefs.h"
-//FlashFS port?
-#elif defined(USE_FLASHFS)
-   #include "fs_port_flashFS.h"
 //SPIFFS port?
 #elif defined(USE_SPIFFS)
    #include "fs_port_spiffs.h"
