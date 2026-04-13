cccc0114cbea626ff91ef166404884d275c8d610
engZafer35 <eng.zafer.s@gmail.com>
2026-04-13 12:43:08 +0300

    [DEV]-added generator_network_service.py

--- Customers/NetworkService_Config.h
+++ Customers/NetworkService_Config.h
@@ -1,16 +1,15 @@
 /**
- * @file Protocol_Config.h
- * @brief AUTO-GENERATED configuration for Protocol
+ * @file NetworkService_Config.h
+ * @brief AUTO-GENERATED configuration for Network Service
  *        AUTO-GENERATED FILE - DO NOT EDIT MANUALLY
  */
- #ifndef __APP_NETWORK_CONFIG_H__
- #define __APP_NETWORK_CONFIG_H__
- 
- /* generated on: 2026-04-09 10:13:55 */
- /* customer name: LinuxGcc */
- 
- #include "LinuxGcc/Network/Cus_Network_Config.h"
- 
- 
- #endif /* __APP_NETWORK_CONFIG_H__ */
- 
+#ifndef __APP_NETWORK_SERVICE_CONFIG_H__
+#define __APP_NETWORK_SERVICE_CONFIG_H__
+
+/* generated on: 2026-04-13 12:39:36 */
+/* customer name: LinuxGcc */
+
+#include "LinuxGcc/Network/Cus_Network_Config.h"
+
+
+#endif /* __APP_NETWORK_SERVICE_CONFIG_H__ */
