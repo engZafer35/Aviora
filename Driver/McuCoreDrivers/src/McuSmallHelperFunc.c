8272e8158ef0823a8a453d43e839e67653b72876
engZafer35 <eng.zafer.s@gmail.com>
2026-06-04 00:41:05 +0300

    [DEV]-moved uart _write func into mcusmallhelper

---
+++ Driver/McuCoreDrivers/src/McuSmallHelperFunc.c
@@ -0,0 +1,8 @@
+
+#include "Project_Conf.h"
+/* USER CODE BEGIN 1 */
+int _write(int file, char *ptr, int len)
+{
+    HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, HAL_MAX_DELAY);
+    return len;
+}
